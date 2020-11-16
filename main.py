296d52ba6398bdaaa7e031207c44cee419d4af7d
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2020-11-16 05:27:57 +0300

    Create ui.py and main.py
    ui.py has UI of all windows in my app. I didn't convert layoutGame.ui yet because I continue to edit this file live. When I finish development I will convert it.
    
    There are Ui_GameOver, Ui_ConfirmExit, Ui_ConfirmAgain, Ui_ResultTable, Ui_DeleteResult, Ui_ConfirmDeleteAll, Ui_About in ui.py. This file connect with main.py. In main.py you can see the same structure of classes with the same names.

---
+++ main.py
@@ -0,0 +1,334 @@
+import random
+import sqlite3
+import sys
+
+from datetime import datetime
+from typing import List
+
+from PyQt5 import QtCore, uic
+from PyQt5.QtGui import QPixmap, QMouseEvent
+from PyQt5.QtWidgets import (QApplication, QDialog, QHeaderView, QInputDialog,
+                             QMainWindow, QTableWidget, QTableWidgetItem, QWidget)
+
+from ui import (Ui_About, Ui_ConfirmAgain, Ui_ConfirmDeleteAll, Ui_ConfirmExit,
+                Ui_DeleteResult, Ui_GameOver, Ui_ResultsTable)
+
+PRICES = [
+    0, 500, 1000, 2000, 3000, 5000, 10000,
+    15000, 25000, 50000, 100000, 200000,
+    400000, 800000, 1500000, 3000000
+]
+GUARANTEED_PRICES = ['0'] * 5 + ['5,000'] * 5 + ['100,000'] * 5
+
+
+def sql_request(request: str):
+    with sqlite3.connect('database.sqlite3') as con:
+        cur = con.cursor()
+        if 'select' in request.lower():
+            try:
+                return cur.execute(request).fetchall()
+            except Exception as ex:
+                return 'ERROR: ' + str(ex)
+        else:
+            try:
+                cur.execute(request)
+                con.commit()
+                return 'OK'
+            except Exception as ex:
+                return 'ERROR: ' + str(ex)
+
+
+def get_questions():
+    questions_data, questions = [sql_request(
+        'SELECT * FROM "{}_questions"'.format(i)
+    ) for i in range(1, 16)], []
+
+    for q_unshuffled in questions_data:
+        questions_set = []
+
+        q_shuffled = q_unshuffled.copy()
+        random.shuffle(q_shuffled)
+        for q in q_shuffled[:2]:
+            text, corr_answ, answs = q[1], q[2], list(q[2:])
+            random.shuffle(answs)
+            questions_set.append([text, corr_answ, answs])
+
+        questions.append(questions_set)
+
+    return questions
+
+
+def makeTable(table: QTableWidget, header: List[str], data: List[List[str]]):
+    table.setColumnCount(len(header))
+    table.setHorizontalHeaderLabels(header)
+    table.setRowCount(0)
+
+    for i, rowlist in enumerate(data):
+        table.setRowCount(table.rowCount() + 1)
+        for j, elem in enumerate(rowlist):
+            table.setItem(i, j, QTableWidgetItem(elem))
+    table.resizeColumnsToContents()
+
+    table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
+    table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+    table.setEditTriggers(QTableWidget.NoEditTriggers)
+
+
+class StartWindow(QInputDialog):
+    '''
+    StartWindow\n
+    • type: QInputDialog\n
+    • target: enter player's name 
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.getName('Введите ваше имя:')
+
+    def getName(self, shown_text):
+        name, is_accepted = self.getText(self, ' ', shown_text)
+        self.saveGeometry
+        if is_accepted:
+            if not name:
+                self.close()
+                self.getName('Пожалуйста, введите имя:')
+                return 0
+            self.startGame(name)
+        else:
+            sys.exit()
+
+    def startGame(self, name):
+        self.game = GameWindow(name)
+        self.game.show()
+
+        self.game.lost_change.hide()
+        self.game.lost_5050.hide()
+        self.game.lost_x2.hide()
+
+        self.close()
+
+
+class GameWindow(QMainWindow):
+    '''
+    GameWindow\n
+    • type: QMainWindow\n
+    • target: playing the game 
+    '''
+
+    def __init__(self, name=''):
+        super().__init__()
+        uic.loadUi('ui/layoutGame.ui', self)
+        self.control, self.name, self.date = False, name, datetime.today().strftime('%d.%m.%Y %H:%M')
+
+        self.timer = 900
+        self.is_x2_now, self.non_active = False, []
+        self.lifelines = [True, True, True]
+
+        self.new_game.triggered.connect(self.openConfirmAgain)
+        self.close_game.triggered.connect(self.openConfirmClose)
+        self.about.triggered.connect(self.openAbout)
+        self.open_table.triggered.connect(self.openTable)
+        self.clear_one.triggered.connect(self.openDeleteResultForm)
+        self.clear_all.triggered.connect(self.openConfirmDeleteAll)
+        self.setFixedSize(1100, 703)
+
+    def showGameOver(self, data):
+        self.game_over = GameOverWindow(self, data)
+        self.game_over.move(169 + self.x(), 210 + self.y())
+        self.game_over.show()
+
+    def openConfirmAgain(self):
+        self.confirm_wndw = ConfirmAgainWindow(self)
+        self.confirm_wndw.move(169 + self.x(), 210 + self.y())
+        self.confirm_wndw.show()
+
+    def openConfirmClose(self):
+        self.confirm_wndw = ConfirmCloseWindow()
+        self.confirm_wndw.move(169 + self.x(), 210 + self.y())
+        self.confirm_wndw.show()
+
+    def openTable(self):
+        self.results_table = ResultsTableWindow()
+        self.results_table.move(169 + self.x(), 93 + self.y())
+        self.results_table.show()
+
+    def openDeleteResultForm(self):
+        self.delete_form = DeleteResultWindow()
+        self.delete_form.move(150 + self.x(), 93 + self.y())
+        self.delete_form.show()
+
+    def openConfirmDeleteAll(self):
+        self.confirm_wndw = ConfirmDeleteAll()
+        self.confirm_wndw.move(169 + self.x(), 210 + self.y())
+        self.confirm_wndw.show()
+
+    def openAbout(self):
+        self.about = AboutWindow()
+        self.about.move(175 + self.x(), 180 + self.y())
+        self.about.show()
+
+
+class GameOverWindow(QDialog, Ui_GameOver):
+    def __init__(self, parent, data):
+        super().__init__()
+        self.setupUi(self)
+        self.parent = parent
+        self.buttonBox.accepted.connect(self.restart)
+        self.buttonBox.rejected.connect(self.exit)
+        self.label.setText(self.label.text().replace('{0}', data[0]).replace('{1}', data[1]))
+
+    def restart(self):
+        self.parent.restartGame()
+        self.close()
+        self.results = ResultsTableWindow()
+        self.results.move(169 + self.parent.x(), 93 + self.parent.y())
+        self.results.show()
+    
+    def exit(self):
+        self.parent.close()
+        self.close()
+        self.results = ResultsTableWindow()
+        self.results.show()
+
+
+class ConfirmAgainWindow(QDialog, Ui_ConfirmAgain):
+    '''
+    ConfirmAgainWindow\n
+    • type: QDialog\n
+    • target: сonfirm restarting game
+    '''
+
+    def __init__(self, parent: GameWindow):
+        super().__init__()
+        self.setupUi(self)
+        self.parent = parent
+        self.accepted.connect(self.restart)
+        self.rejected.connect(self.close)
+
+    def restart(self):
+        self.parent.restartGame()
+        self.close()
+
+
+class ConfirmCloseWindow(QDialog, Ui_ConfirmExit):
+    '''
+    ConfirmCloseWindow\n
+    • type: QDialog\n
+    • target: сonfirm player's exit from the app
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.accepted.connect(sys.exit)
+        self.rejected.connect(self.close)
+        self.setFixedSize(400, 140)
+
+
+class ResultsTableWindow(QWidget, Ui_ResultsTable):
+    '''
+    ResultsTableWindow\n
+    • type: QWidget\n
+    • target: show the results
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        results = sql_request('SELECT * from results')
+        results = sorted(results, key=lambda x: int(str(x[2]).replace(',', '')), reverse=True)
+        results = [list(map(str, i[1:])) for i in results]
+
+        makeTable(self.tableWidget, ['Имя', 'Результат', 'Дата и время'], results)
+        self.okButton.clicked.connect(self.close)
+
+
+class DeleteResultWindow(QWidget, Ui_DeleteResult):
+    '''
+    DeleteResultWindow\n
+    • type: QWidget\n
+    • target: delete one result from the result's table
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.refreshTable()
+        self.deleteButton.clicked.connect(self.deleteAction)
+
+    def refreshTable(self):
+        results = sql_request('SELECT * from results')
+        results = sorted(results, key=lambda x: int(str(x[2]).replace(',', '')), reverse=True)
+        results = [list(map(str, i[1:])) for i in results]
+        self.results = [list(map(str, [i + 1, results[i][2]])) for i in range(len(results))]
+
+        self.spinBox.setMinimum(1)
+        self.spinBox.setMaximum(len(results))
+
+        makeTable(self.tableWidget, ['Имя', 'Результат', 'Дата и время'], results)
+
+    def deleteAction(self):
+        id_result = self.spinBox.text()
+        result_date = list(filter(lambda x: x[0] == id_result, self.results))[0][1]
+        result = sql_request('DELETE FROM results WHERE date = "{}"'.format(result_date))
+        if 'ERROR' in result:
+            print(result)
+        self.refreshTable()
+
+
+class ConfirmDeleteAll(QDialog, Ui_ConfirmDeleteAll):
+    '''
+    ConfirmDeleteAll\n
+    • type: QDialog\n
+    • target: сonfirm deleting all data from the result's table
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.accepted.connect(self.deleteAllData)
+        self.rejected.connect(self.close)
+        self.setFixedSize(400, 140)
+
+    def deleteAllData(self):
+        result = sql_request('DELETE FROM results')
+        self.results_table = ResultsTableWindow()
+        self.results_table.show()
+        self.close()
+
+
+class AboutWindow(QWidget, Ui_About):
+    '''
+    AboutWindow\n
+    • type: QWidget\n
+    • target: tell about app's author
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.enText.hide()
+        self.ruButton.clicked.connect(self.showRuText)
+        self.enButton.clicked.connect(self.showEnText)
+        self.okButton.clicked.connect(self.hide)
+        self.setFixedSize(380, 222)
+
+    def showRuText(self):
+        self.ruText.show()
+        self.enText.hide()
+
+    def showEnText(self):
+        self.enText.show()
+        self.ruText.hide()
+
+
+def main():
+    app = QApplication(sys.argv)
+    wndw = StartWindow()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
---
+++ ui.py
@@ -0,0 +1,337 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_GameOver(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 150)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        Dialog.setFont(font)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(Dialog)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(Dialog)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(
+            QtWidgets.QDialogButtonBox.No | QtWidgets.QDialogButtonBox.Yes)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.verticalLayout.addWidget(self.buttonBox)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Вы проиграли!"))
+        self.label.setText(_translate("Dialog", "Вы проиграли! Правильный ответ: {0}.\n"
+                                      "Ваш выигрыш составил {1} руб.\n"
+                                      "Начать новую игру?"))
+
+
+class Ui_ConfirmExit(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 140)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        Dialog.setFont(font)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(30, 90, 341, 32))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        self.buttonBox.setFont(font)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(
+            QtWidgets.QDialogButtonBox.No | QtWidgets.QDialogButtonBox.Yes)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(24, 20, 351, 50))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Завершить игру?"))
+        self.label.setText(_translate(
+            "Dialog", "Действительно завершить текущую игру без сохранения?"))
+
+
+class Ui_ConfirmAgain(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.setFixedSize(400, 140)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        Dialog.setFont(font)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(30, 90, 341, 32))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        self.buttonBox.setFont(font)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(
+            QtWidgets.QDialogButtonBox.No | QtWidgets.QDialogButtonBox.Yes)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(24, 20, 351, 50))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Начать новую игру?"))
+        self.label.setText(_translate(
+            "Dialog", "Действительно начать новую игру без сохранения текущей?"))
+
+
+class Ui_ResultsTable(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(395, 385)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        Form.setFont(font)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(Form)
+        self.horizontalLayout.setContentsMargins(10, 10, 10, 10)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.tableWidget = QtWidgets.QTableWidget(Form)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.verticalLayout.addWidget(self.tableWidget)
+        self.okButton = QtWidgets.QPushButton(Form)
+        self.okButton.setObjectName("okButton")
+        self.verticalLayout.addWidget(self.okButton)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Таблица результатов"))
+        self.okButton.setText(_translate("Form", "ОК"))
+
+
+class Ui_DeleteResult(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(442, 385)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setContentsMargins(10, 10, 10, 10)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.tableWidget = QtWidgets.QTableWidget(Form)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        self.tableWidget.setFont(font)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.verticalLayout.addWidget(self.tableWidget)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setSpacing(10)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.spinBox = QtWidgets.QSpinBox(Form)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        self.spinBox.setFont(font)
+        self.spinBox.setObjectName("spinBox")
+        self.horizontalLayout.addWidget(self.spinBox)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.deleteButton = QtWidgets.QPushButton(Form)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        self.deleteButton.setFont(font)
+        self.deleteButton.setObjectName("deleteButton")
+        self.verticalLayout.addWidget(self.deleteButton)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Удаление результата из таблицы"))
+        self.label.setText(_translate("Form", "Выберите номер результата для удаления:"))
+        self.deleteButton.setText(_translate(
+            "Form", "Удалить (без подтверждений, сразу при нажатии)"))
+
+
+class Ui_ConfirmDeleteAll(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 140)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        Dialog.setFont(font)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(30, 90, 341, 32))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        self.buttonBox.setFont(font)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(
+            QtWidgets.QDialogButtonBox.No | QtWidgets.QDialogButtonBox.Yes)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(24, 20, 351, 50))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Очистить таблицу результатов?"))
+        self.label.setText(_translate(
+            "Dialog", "Действительно очистить всю таблицу результатов без возможности вернуть?"))
+
+
+class Ui_About(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(380, 308)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(Form)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.ruText = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        self.ruText.setFont(font)
+        self.ruText.setScaledContents(False)
+        self.ruText.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
+        self.ruText.setWordWrap(True)
+        self.ruText.setObjectName("ruText")
+        self.verticalLayout.addWidget(self.ruText)
+        self.enText = QtWidgets.QLabel(Form)
+        self.enText.setEnabled(True)
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        font.setPointSize(9)
+        self.enText.setFont(font)
+        self.enText.setScaledContents(False)
+        self.enText.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
+        self.enText.setWordWrap(True)
+        self.enText.setObjectName("enText")
+        self.verticalLayout.addWidget(self.enText)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.ruButton = QtWidgets.QPushButton(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.ruButton.sizePolicy().hasHeightForWidth())
+        self.ruButton.setSizePolicy(sizePolicy)
+        self.ruButton.setMaximumSize(QtCore.QSize(50, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        self.ruButton.setFont(font)
+        self.ruButton.setObjectName("ruButton")
+        self.horizontalLayout.addWidget(self.ruButton)
+        self.enButton = QtWidgets.QPushButton(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.enButton.sizePolicy().hasHeightForWidth())
+        self.enButton.setSizePolicy(sizePolicy)
+        self.enButton.setMaximumSize(QtCore.QSize(50, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        self.enButton.setFont(font)
+        self.enButton.setObjectName("enButton")
+        self.horizontalLayout.addWidget(self.enButton)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.okButton = QtWidgets.QPushButton(Form)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.okButton.sizePolicy().hasHeightForWidth())
+        self.okButton.setSizePolicy(sizePolicy)
+        self.okButton.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("PT Sans")
+        self.okButton.setFont(font)
+        self.okButton.setObjectName("okButton")
+        self.verticalLayout_2.addWidget(self.okButton)
+        self.verticalLayout.addLayout(self.verticalLayout_2)
+        self.horizontalLayout_2.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "О программе"))
+        self.ruText.setText(_translate("Form", "<html><head/><body><p align=\"center\"><span style=\" font-weight:600;\">Игра «Кто хочет стать Миллионером?»</span></p><p align=\"center\">Игра, написанная на Python и основанная на популярном интеллектуальном телешоу <br/>«Who Wants to Be a Millionaire?»</p><p align=\"center\">© Павел Овчинников, 2020</p></body></html>"))
+        self.enText.setText(_translate("Form", "<html><head/><body><p align=\"center\"><span style=\" font-weight:600;\">The Game «Who Want to Be a Millionaire?»</span></p><p align=\"center\">The game was written on Python and based on popular intellectual TV show <br/>«Who Wants to Be a Millionaire?»</p><p align=\"center\">© Pavel Ovchinnikov, 2020</p></body></html>"))
+        self.ruButton.setText(_translate("Form", "RU"))
+        self.enButton.setText(_translate("Form", "EN"))
+        self.okButton.setText(_translate("Form", "ОК"))
