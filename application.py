e8b8151b7e3c8826b2da5bcbf83ddb9b646a76a3
PaveTranquil <opavel@internet.ru>
2021-10-25 06:47:39 +0300

    refactor: распределили всё по файлам
    Файлов становится много, необходим рефакторинг.
    - QApplication и MessageBox с ошибкой перенесли в application.py
    - Все функции, запускающиеся самостоятельно, отправили в funcs.py
    - Подвели все необходимые импорты
    
    main.py стал легче на 124 строки.

---
+++ application.py
@@ -0,0 +1,15 @@
+import sys
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMessageBox
+
+app = QApplication(sys.argv)
+
+msg = QMessageBox()
+msg.setWindowTitle("Упс, ошибка...")
+msg.setWindowIcon(QIcon('images/app_icon.ico'))
+msg.setIcon(QMessageBox.Critical)
+msg.setText('<html><body><p>Кажется, мы споткнулись об какую-то ошибку.\n'
+            'Чтобы прокачать игру, отправьте файл logs.txt <a href="https://t.me/pavetranquil">'
+            '<span style="text-decoration: underline; color:#005ffe; font-weight:600;">разработчику'
+            '</span></a>.</p></body></html>')
--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import logging
-import sqlite3
 import sys
-import traceback  # для получения текстового представления исключения
 from datetime import datetime  # обозначить время начало игры
 from functools import partial  # для передачи аргументов в функцию без её вызова
 from os.path import realpath  # возвращает глобальный путь
 from random import shuffle
-from types import TracebackType
-from typing import Callable, Type
+from typing import Callable
 
-from PyQt5.QtCore import Qt, QTimer, QUrl
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QCloseEvent, QIcon, QKeyEvent, QMouseEvent, QPixmap
-from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
-from PyQt5.QtWidgets import (QApplication, QDialog, QHeaderView, QMainWindow,
-                             QMessageBox, QTableWidget, QTableWidgetItem,
-                             QWidget)
+from PyQt5.QtMultimedia import QMediaPlayer
+from PyQt5.QtWidgets import QDialog, QMainWindow, QMessageBox, QWidget
 
+from application import app
+from funcs import (decorate_audio, excepthook, get_questions, makeTable,
+                   sql_request)
 from ui import (AnimationLabel, Ui_About, Ui_ConfirmAgain, Ui_ConfirmClearAll,
                 Ui_ConfirmExit, Ui_ConfirmLeave, Ui_DeleteResult, Ui_GameOver,
                 Ui_MainWindow, Ui_ResultsTable, Ui_Rules, Ui_StartDialog,
@@ -29,94 +27,6 @@ PRICES = [
 GUARANTEED_PRICES = ['0'] * 5 + ['5 000'] * 5 + ['100 000'] * 5  # несгораемые суммы
 logging.basicConfig(filename=realpath('logs.txt'), level=logging.DEBUG,
                     format='%(levelname)s: %(message)s')  # конфиг логгирования
-
-
-def sql_request(request: str) -> str:
-    '''Метод для запроса данных из базы данных database.sqlite3 и возвращает "OK" или "ERROR" с описанием ошибки
-
-    Параметры
-    ---------
-    request: str
-        текст запроса
-    '''
-
-    with sqlite3.connect('database.sqlite3') as con:  # подключаемся к бд
-        cur = con.cursor()  # и задаём курсор по бд
-        try:
-            # выясняем цель запроса
-            if 'select' in request.lower():  # для получения данных из бд?
-                return cur.execute(request).fetchall()  # возвращаем выборку
-            else:  # для записи данных в бд?
-                cur.execute(request)  # распаковываем запрос
-                con.commit()
-                return 'OK'
-        except Exception as ex:
-            return 'ERROR: ' + str(ex)  # если в запросе ошибка, возвращаем её
-
-
-def get_questions():
-    '''Метод для получения и подготовки вопросов из базы данных database.sqlite3 для игры
-    '''
-
-    questions_data, questions = [sql_request(
-        'SELECT * FROM "{}_questions"'.format(i)
-    ) for i in range(1, 16)], []  # извлекаем все вопросы из бд
-
-    for q_unshuffled in questions_data:  # проходимся по списку с вопросами по каждому шагу денежного дерева
-        questions_set = []
-
-        q_shuffled = q_unshuffled.copy()
-        shuffle(q_shuffled)  # перемешиваем список вопросов
-        for q in q_shuffled[:2]:  # берём первые два вопроса
-            text, corr_answ, answs = q[1], q[2], list(q[2:])  # отбираем текст вопроса, правильный и другие три ответа
-            shuffle(answs)  # перемешиваем ответы
-            questions_set.append([text, corr_answ, answs])  # собираем два вопроса — первый и для смены вопроса
-
-        questions.append(questions_set)
-
-    logging.info('Qs are gotten')
-    return questions
-
-
-def makeTable(table: QTableWidget, header: list[str], data: list[list[str]]) -> None:
-    '''Метод для генерации таблицы в table с первой сторокой header и матрицей data
-
-    Параметры
-    ---------
-    table: QTableWidget
-        виджет таблицы из Qt
-    header: list[str]
-        первая строка таблицы — её заголовок
-    data: list[list[str]]
-        оставшиеся данные таблицы
-    '''
-
-    table.setColumnCount(len(header))
-    table.setHorizontalHeaderLabels(header)
-    table.setRowCount(0)
-
-    for i, rowlist in enumerate(data):  # заносим контент из data в table
-        table.setRowCount(table.rowCount() + 1)
-        for j, elem in enumerate(rowlist):
-            table.setItem(i, j, QTableWidgetItem(elem))
-    table.resizeColumnsToContents()
-
-    table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
-    table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
-    table.setEditTriggers(QTableWidget.NoEditTriggers)
-
-
-def decorate_audio(file: str) -> QMediaContent:
-    '''Метод, декорирующий путь к аудиофайлу в медиа-контент, понятный для Qt
-
-    Параметры
-    ---------
-    file: str
-        относительный путь к аудиофайлу
-    '''
-
-    url = QUrl.fromLocalFile(file)  # определяем из относительного пути file QUrl-объект, понятный для QMediaContent
-    return QMediaContent(url)  # создаём из QUrl медиа-контент, понятный для Qt, и возвращаем его
 
 
 class StartWindow(QDialog, Ui_StartDialog):
@@ -1268,46 +1178,12 @@ class AboutWindow(QWidget, Ui_About):
         return super().closeEvent(event)
 
 
-def excepthook(exc_type: Type[BaseException], exc_value: BaseException, exc_tb: TracebackType):
-    '''Обработчик исключений
-
-    При вызове исключения логгирует ошибку в логи и показывает окно, предлагающее отправить ошибку разработчику.
-
-    Параметры
-    ---------
-    exc_type: Type[BaseException]
-        тип вызванного исключения
-    exc_value: BaseException
-        описание исключения
-    exc_tb: TracebackType
-        подробный трейсбек
-    '''
-
-    global app, msg
-    logging.error(
-        str(exc_value) + '\n' +
-        ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
-    )  # логируем ошибку
-    msg.show()
-    msg.buttonClicked.connect(app.quit)  # привязываем кнопку «ОК» к завершению приложения
-
-
 if __name__ == '__main__':
     sys.excepthook = excepthook
 
-    app = QApplication(sys.argv)
     logging.info(datetime.today().strftime('%Y-%m-%d %H:%M:%S') + ': Session start')
     wndw = StartWindow()
     wndw.show()
-
-    msg = QMessageBox()
-    msg.setWindowTitle("Упс, ошибка...")
-    msg.setWindowIcon(QIcon('images/app_icon.ico'))
-    msg.setIcon(QMessageBox.Critical)
-    msg.setText('<html><body><p>Кажется, мы споткнулись об какую-то ошибку.\n'
-                'Чтобы прокачать игру, отправьте файл logs.txt <a href="https://t.me/pavetranquil">'
-                '<span style="text-decoration: underline; color:#005ffe; font-weight:600;">разработчику'
-                '</span></a>.</p></body></html>')
 
     app.exec()
     logging.info('Session finish\n\n')
